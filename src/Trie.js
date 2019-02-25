04434f17749099f6deeaa319d9e892df9fde0044
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-25 14:44:46 -0800

    Write trie in js

---
+++ src/Trie.js
@@ -0,0 +1,86 @@
+export default class Trie {
+  constructor(wordList = undefined) {
+    this.id = 0;
+    this.root = ['', this.id, false];
+    this.id += 1;
+    this.dict = [];
+    this.dict[this.root] = [];
+
+    if (wordList !== undefined) {
+      wordList.forEach(word => this.addWord(word));
+    }
+  }
+
+  addWord(word) {
+    let currNode = this.root;
+    const letterIter = word.entries();
+    while (!letterIter.done) {
+      const [i, letter] = letterIter.value;
+      let foundLetter = false;
+      const childIter = this.dict[currNode].entries();
+      while (!childIter.done) {
+        const [j, node] = childIter.value;
+        if (node[0] === letter) {
+          if (letter === word.length - 1 && !node[2]) {
+            this.dict[currNode][j] = [currNode[0], currNode[1], true];
+            currNode = this.dict[currNode][j];
+            this.dict[currNode] = this.dict[node];
+            delete this.dict[node];
+          } else {
+            currNode = node;
+          }
+          foundLetter = true;
+          break;
+        }
+        childIter.next();
+      }
+      if (!foundLetter) {
+        let newNode;
+        if (i === word.length - 1) {
+          newNode = [letter, this.id, true];
+        } else {
+          newNode = [letter, this.id, false];
+        }
+        this.id += 1;
+        this.dict[currNode].push(newNode);
+        this.dict[newNode] = [];
+        currNode = newNode;
+      }
+      letterIter.next();
+    }
+  }
+
+  findWordsFromNode(words, prefix = '', node = this.root) {
+    if (node[2]) {
+      words.push(prefix);
+    }
+
+    this.dict[node].forEach((child) => {
+      const newPrefix = prefix + child[0];
+      this.findWordsFromNode(words, newPrefix, child);
+    });
+  }
+
+  autocomplete(prefix) {
+    const words = [];
+    let currNode = this.root;
+
+    prefix.forEach((letter) => {
+      let foundLetter = false;
+
+      this.dict[currNode].forEach((node) => {
+        if (node[0] === letter && !foundLetter) {
+          currNode = node;
+          foundLetter = true;
+        }
+      });
+      if (!foundLetter) {
+        console.error('PREFIX NOT IN TRIE');
+        return words; 
+      }
+    });
+    const baseNode = currNode;
+    this.findWordsFromNode(words, prefix, baseNode);
+    return words;
+  }
+}
