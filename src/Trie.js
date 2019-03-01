b0bd62f19b27748effab0d2837a20fcce34258c4
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-28 20:40:09 -0800

    Begin creating renderNewNode function

--- src/Trie.js
+++ src/Trie.js
@@ -8,12 +8,13 @@ export default class Trie extends Component {
       root: [false, 0, false],
       id: 1,
       dict: [],
-      childrenPerLevel: []
+      renderedNodes: []
     };
     this.state.dict[this.state.root] = [];
   }
 
   addWord(word) {
+    let prevNode;
     let currNode = this.state.root;    
     
     const letterIter = word.split('').entries();
@@ -27,21 +28,18 @@ export default class Trie extends Component {
       let childResult = childIter.next();
       
       while (!childResult.done) {
-        console.log('childResult', childResult);
-
 
         const [j, node] = childResult.value;
         if (node[0] === letter) {
-          if (letter === word.length - 1 && !node[2]) {
+          if (i === word.length - 1 && !node[2]) {
             /* eslint-disable-next-line no-loop-func*/
-            this.setState({dict:this.state.dict.map()})
             this.setState((state) => {
               state.dict[currNode][j] = [currNode[0], currNode[1], true];
               return { dict: state.dict }
             })
             // this.state.dict[currNode][j] = [currNode[0], currNode[1], true];
+            prevNode = currNode;
             currNode = this.state.dict[currNode][j];
-            console.log("currNode", currNode);
             
             
             /* eslint-disable-next-line no-loop-func*/
@@ -56,9 +54,8 @@ export default class Trie extends Component {
             })
             // delete this.state.dict[node];
           } else {
+            prevNode = currNode;
             currNode = node;
-            console.log("currNode", currNode);
-            
           }
           foundLetter = true;
           break;
@@ -72,48 +69,32 @@ export default class Trie extends Component {
         } else {
           newNode = [letter, this.state.id, false];
         }
-        this.setState({ id: this.state.id + 1 })
-        console.log("about to add node");
+        console.log("id before:", this.state.id);
+        
+        const updateId = (state) => {
+          state.id += 1
+
+          return { id: state.id }
+        }
+        this.setState(updateId(this.state));
+
+        console.log("id after:", this.state.id);
+        
         /* eslint-disable-next-line no-loop-func*/
         const updateDict = (state) => {
-          console.log("current letter", letter)
-          console.log("currNode", currNode)
-          console.log("state.dict", state.dict);
-          console.log("state.dict[currNode]", state.dict[currNode]);
           state.dict[currNode].push(newNode)
-          console.log('in set state', state.dict);
           state.dict[newNode] = [];
-          if(state.childrenPerLevel[i]){
-            state.childrenPerLevel[i] += 1;
-          } else {
-            state.childrenPerLevel[i] = 1
-          }
           return { dict: state.dict } 
         }
         this.setState(updateDict(this.state));
-        console.log('saved in dict', this.state.dict);
-        console.log('num children', this.state.childrenPerLevel)
-        console.log('ref', this.refs)
-        d3.select(this.refs.svg).append("circle")
-            .attr("cx", 60 * (this.state.childrenPerLevel[i]))
-            .attr("cy", 50 * (i + 1))
-            .attr("r", 20)
-            .style("fill", "white")
-            .style("stroke-width", 2)
-            .style('stroke', 'black');
-        d3.select(this.refs.svg).append("text")
-        .attr("x", 55 * (this.state.childrenPerLevel[i]))
-        .attr("y", 50 * (i + 1))
-        .text(letter)
-        .style("font-size", "25px")
-        .style("fill", "black")
         
+        this.renderNewNode(currNode, newNode, i);
 
         
         // this.state.dict[currNode].push(newNode);
         // this.state.dict[newNode] = [];
+        prevNode = currNode;
         currNode = newNode;
-        console.log("currNode", currNode);
       }
       letterResult = letterIter.next();
     }
@@ -130,12 +111,81 @@ export default class Trie extends Component {
     });
   }
 
+  renderNewNode(prevNode, node, level) {
+    // TODO: Save the coupling of the circle and text to an array
+        //          this will allow me to alter the rendering of the
+        //          circles after intial rendering (allowing for shifting)
+        // TODO: Save the parent circle, this will allow for drawing the
+        //          line from parent to child. This needs to be svg and
+        //          not the node data. This is because the x and y will
+        //          change later if more nodes at the height are rendered.
+        //          BUT, what if the previous node wasn't just created?
+        //          How do we pass through the circle? We don't just send
+        //          the node, when inserting the rendered objects into 
+        //          renderedNodes also insert the nodes themselves so
+        //          when we go to write the line you can search for the
+        //          node in the renderedNodes collection.
+        // TODO: Maybe make a d3 render method. At render time check 
+        //          to see if there are any other nodes on the level.
+        //          if there are, change the rendering of the other nodes
+        //          then insert the new node
+        // TODO: When rendering the nodes on a level, consider the
+        //          entire level as 100% width. Once implemented consider
+        //          switching to giving each parent node a specified 
+        //          children width.  Then use that as 100%
+        if(this.state.renderedNodes[level]){
+          const newNumOnLevel = this.state.renderedNodes[level].length + 1
+        } else {
+          const newNumOnLevel = 1
+        }
+        let color = "#f3f3f3ff"
+        if (node[2]) {
+          color = "#fce5cdff"
+        }
+        let circle = d3.select(this.refs.svg).append("circle")
+            .attr("cx", 60 * (this.state.renderedNodes[level].length))
+            .attr("cy", 50 * (level + 2))
+            .attr("r", 20)
+            .style("fill", color);
+        let text = d3.select(this.refs.svg).append("text")
+          .attr("x", -12 + 60 * (this.state.renderedNodes[level].length))
+          .attr("y", 5 + 50 * (level + 2))
+          .text("'" + node[0] + "'")
+          .style("font-size", "25px")
+          .style("fill", "black");
+        
+        const updateRenderedNodes = ((state) => {
+          if(state.renderedNodes[level]){
+            state.renderedNodes[level].push([node, circle, text])
+          } else {
+            state.renderedNodes[level] =[node, circle, text] 
+          }
+          return {renderedNodes: state.renderedNodes};
+        });
+
+        this.setState(updateRenderedNodes(this.state));
+  }
+
   componentDidMount() {
-    this.addWord('test')
-    this.addWord('ternary')
-    this.addWord('testical')
-    this.addWord('terrible')
-    this.addWord('tim')
+    d3.select(this.refs.svg).append("circle")
+            .attr("cx", 60 * 3)
+            .attr("cy", 50)
+            .attr("r", 20)
+            .style("fill", "#f3f3f3ff");
+        d3.select(this.refs.svg).append("text")
+        .attr("x", -5 + 60 * 3)
+        .attr("y", 5 + 50)
+        .text("''")
+        .style("font-size", "25px")
+        .style("fill", "black")
+    this.addWord('matt')
+    this.addWord('mattress')
+    this.addWord('matthew')
+    this.addWord('matilda')
+    this.addWord('mathias')
+    this.addWord('matromoney')
+    console.log(this.state.dict);
+    
   }
 
   autocomplete(prefix) {
