bdf1a4fe085ac7fa72662b0c7b506daaa114b919
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-08 13:36:08 -0800

    Updates

---
+++ precache-manifest.20baa043fcf4ce69fbec3e073df52a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "177db53c832afebf5442",
+    "url": "/static/js/main.177db53c.chunk.js"
+  },
+  {
+    "revision": "63042839f241f7c2a40e",
+    "url": "/static/js/2.63042839.chunk.js"
+  },
+  {
+    "revision": "177db53c832afebf5442",
+    "url": "/static/css/main.285fbaff.chunk.css"
+  },
+  {
+    "revision": "62d4ee80fd43be864a374a5bfe1c0c17",
+    "url": "/index.html"
+  }
+];
